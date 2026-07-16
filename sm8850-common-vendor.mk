af791f92595703f1c5d6a4236a98a5f1c821229f
chandu078 <chandudyavanapelli03@gmail.com>
2026-07-16 20:29:52 +0000

    sm8850-common: De-blob vendor.oplus.hardware.displaycolorfeature V1 interface
    Change-Id: Ib17047c4e6922542db1f5ce9e6a88806c45b5c73

--- sm8850-common-vendor.mk
+++ sm8850-common-vendor.mk
@@ -818,7 +818,6 @@ PRODUCT_PACKAGES += \
     libquasar \
     libvolumelistener \
     sva_plugin \
-    vendor.oplus.hardware.displaycolorfeature-V1-ndk_vendor \
     vendor.pixelworks.hardware.display-V3-ndk \
     vendor.pixelworks.hardware.display@1.0_vendor \
     vendor.pixelworks.hardware.display@1.1_vendor \
@@ -951,7 +950,6 @@ PRODUCT_PACKAGES += \
     libqcodec2_dolbydecoderfilter \
     libsensorbridge \
     libstc_color_feature \
-    vendor.oplus.hardware.displaycolorfeature-V1-ndk \
     vendor.pixelworks.hardware.display@1.0 \
     vendor.pixelworks.hardware.display@1.1 \
     vendor.pixelworks.hardware.display@1.2 \
