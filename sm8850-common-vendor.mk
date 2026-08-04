d3574b622423d934aad8b830ebf9e131afdccc15
1vivy <1vivy@tutanota.com>
2026-08-04 16:47:54 -0600

    sm8850-common: ship URCC runtime closure
    Icp-Layer: bringup
    Icp-Class: upstreamable-general-oneplus
    Icp-Undoes: proprietary_vendor_oneplus_sm8850-common:./sm8850-common-vendor.mk - camera-port base linked open URCC clients without the stock service/config/core runtime closure
    Upstream-Half: Common SM8850 stock URCC service and UAH core runtime paired with the open URCC interface/client path

--- sm8850-common-vendor.mk
+++ sm8850-common-vendor.mk
@@ -17,7 +17,9 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/sm8850-common/proprietary/odm/etc/init/vendor-oplus-hardware-touch-V2-hbp5-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor-oplus-hardware-touch-V2-hbp5-service.rc \
     vendor/oneplus/sm8850-common/proprietary/odm/etc/init/vendor.oplus.hardware.charger-V11-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor.oplus.hardware.charger-V11-service.rc \
     vendor/oneplus/sm8850-common/proprietary/odm/etc/init/vendor.oplus.hardware.displaypanelfeature-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor.oplus.hardware.displaypanelfeature-service.rc \
+    vendor/oneplus/sm8850-common/proprietary/odm/etc/init/vendor.oplus.hardware.urcc-service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor.oplus.hardware.urcc-service.rc \
     vendor/oneplus/sm8850-common/proprietary/odm/etc/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml \
+    vendor/oneplus/sm8850-common/proprietary/odm/etc/uah/uahconfig.pb:$(TARGET_COPY_OUT_ODM)/etc/uah/uahconfig.pb \
     vendor/oneplus/sm8850-common/proprietary/product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/com.android.hotwordenrollment.common.util.xml \
     vendor/oneplus/sm8850-common/proprietary/system/etc/sysconfig/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     vendor/oneplus/sm8850-common/proprietary/system_ext/etc/horae/horae.conf:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/horae/horae.conf \
@@ -952,6 +954,8 @@ PRODUCT_PACKAGES += \
     libqcodec2_dolbydecoderfilter \
     libsharebuffer_impl \
     libstc_color_feature \
+    libuahcore \
+    liburcccore \
     vendor.dolby.dvs-V1-ndk \
     vendor.pixelworks.hardware.display@1.0 \
     vendor.pixelworks.hardware.display@1.1 \
@@ -1024,6 +1028,7 @@ PRODUCT_PACKAGES += \
     manifest_oplus_cwb_aidl.xml \
     manifest_oplus_displaypanelfeature_aidl.xml \
     manifest_touch_aidl.xml \
+    vendor.oplus.hardware.urcc-service.xml \
     vintf_manifext_aidl_panelchaplin.xml \
     C2PAInternetService \
     adsprpcd \
@@ -1137,6 +1142,7 @@ PRODUCT_PACKAGES += \
     vendor-oplus-hardware-touch-V2-hbp5-service \
     vendor.oplus.hardware.charger-V11-service \
     vendor.oplus.hardware.displaypanelfeature-service \
+    vendor.oplus.hardware.urcc-service \
     wlschgd \
     init.oplus.wifi \
     oldc_vnd \
