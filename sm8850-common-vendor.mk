7315f260de10372f7d500a3e01c4b0aea5b5744f
1vivy <1vivy@tutanota.com>
2026-08-06 23:09:17 -0600

    sm8850-common: restore the proprietary display payload
    Restore the full allocator, composer, demura, mapper, SDM, VINTF, and display-boot payload while retaining both AudioX aggregate commits.
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Icp-Layer: bringup
    
    Upstream-Half: none — Track F substrate selection; commits remain live on staging/features-v2/all
    
    Icp-Undoes: proprietary_vendor_oneplus_sm8850-common:8ccc38fd9e124a7fdd4313f1af85f8851a337d37 - supersedes display boot-script payload retirement
    
    Icp-Undoes: proprietary_vendor_oneplus_sm8850-common:8da5181d902d31912e9a783f8c1bd94ab3c6aba4 - supersedes proprietary display payload retirement
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- sm8850-common-vendor.mk
+++ sm8850-common-vendor.mk
@@ -97,6 +97,7 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/hw/init.qti.kernel.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qti.kernel.target.rc \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/init.qccvendor.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qccvendor.rc \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/init.qt-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qt-service.rc \
+    vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/init.qti.display_boot.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qti.display_boot.rc \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/init.qti.media.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qti.media.rc \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/init.qti.qcv.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qti.qcv.rc \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/init.time_daemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.time_daemon.rc \
@@ -123,7 +124,10 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/vendor.qti.hardware.alarm-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.alarm-service.rc \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/vendor.qti.hardware.c2pa-aidl-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.c2pa-aidl-service-qti.rc \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/vendor.qti.hardware.capabilityconfigstore-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.capabilityconfigstore-service.rc \
+    vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/vendor.qti.hardware.display.allocator-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.display.allocator-service.rc \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/vendor.qti.hardware.display.color-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.display.color-service.rc \
+    vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/vendor.qti.hardware.display.composer-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.display.composer-service.rc \
+    vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/vendor.qti.hardware.display.demura-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.display.demura-service.rc \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/vendor.qti.hardware.dsp-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.dsp-service.rc \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/vendor.qti.hardware.minkipcbinder@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.minkipcbinder@1.0-service.rc \
     vendor/oneplus/sm8850-common/proprietary/vendor/etc/init/vendor.qti.hardware.perf2-hal-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.perf2-hal-service.rc \
@@ -384,12 +388,14 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.ranging@aidl-impl-qti \
     android.hardware.bluetooth@1.0-impl-qti \
     android.hardware.bluetooth@aidl-impl-qti \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
     audio.bluetooth_qti.default \
     libaudiocorehal.default \
     libaudiocorehal.qti \
     libaudioeffecthal.qti \
     libqtigatekeeper \
     libsoundtriggerhal.qti \
+    mapper.qti \
     mbedtls_qti \
     qc_bcs_lib \
     vendor.qti.hardware.alarm-impl \
@@ -499,6 +505,8 @@ PRODUCT_PACKAGES += \
     libdiag \
     libdigital-dimming \
     libdisp-aba \
+    libdisplayconfig.qti \
+    libdisplaydebug \
     libdisplayqos \
     libdisplayskuutils \
     libdmap_consent_sdk \
@@ -506,6 +514,7 @@ PRODUCT_PACKAGES += \
     libdpps \
     libdrmfs \
     libdrmtime \
+    libdrmutils \
     libdsd2pcm \
     libdtlm \
     libeai_fixed \
@@ -514,11 +523,16 @@ PRODUCT_PACKAGES += \
     libfastcvdsp_stub \
     libfastcvopt \
     libfeutils \
+    libfilefinder \
     libfmpal \
     libgame_enhance \
     libgamepoweroptfeature \
     libgpt \
+    libgpu_tonemapper \
     libgpudataproducer \
+    libgralloc.qti \
+    libgralloccore \
+    libgrallocutils \
     libgsl \
     libhdcp1prov \
     libhdcp2p2prov \
@@ -530,6 +544,7 @@ PRODUCT_PACKAGES += \
     libhdrdynamicootf \
     libhdrvivid \
     libhfp_pal \
+    libhistogram \
     libhwcsensor \
     libidl \
     libidv \
@@ -549,6 +564,7 @@ PRODUCT_PACKAGES += \
     libloadalgo_stub \
     liblx-ar_util \
     liblx-osal \
+    libmapperutils \
     libmcs \
     libmdmdetect \
     libmdsprpc \
@@ -630,9 +646,11 @@ PRODUCT_PACKAGES += \
     libqcodec2_utils \
     libqcodec2_v4l2codec \
     libqconfigclient \
+    libqdMetaData \
     libqdcm-algo \
     libqdcm-json-mode-parser \
     libqdcm-mode-parser \
+    libqdutils \
     libqesdk2_0 \
     libqesdk_manager \
     libqesdk_qshsession \
@@ -653,6 +671,7 @@ PRODUCT_PACKAGES += \
     libqrtr \
     libqrtrclient \
     libqseed3 \
+    libqservice \
     libqsh \
     libqsh_ble_pb \
     libqsocket \
@@ -685,11 +704,16 @@ PRODUCT_PACKAGES += \
     libscveObjectSegmentation \
     libscveObjectTracker \
     libscveObjectTracker_stub \
+    libsdedrm \
     libsdm-brightnessaware-parser \
     libsdm-color \
     libsdm-colormgr-algo \
     libsdm-disp-vndapis \
+    libsdmclient \
+    libsdmcore \
+    libsdmdal \
     libsdmextension \
+    libsdmutils \
     libsdsprpc \
     libseclog \
     libsensinghubapi-prop \
@@ -816,6 +840,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bttpi-V3-ndk \
     vendor.qti.hardware.c2pa-V1-ndk \
     vendor.qti.hardware.capabilityconfigstore-V1-ndk \
+    vendor.qti.hardware.display.snapalloc-impl \
     vendor.qti.hardware.dsp-V1-ndk \
     vendor.qti.hardware.dsp@1.0 \
     vendor.qti.hardware.fm-V1-ndk \
@@ -965,6 +990,7 @@ PRODUCT_PACKAGES += \
     fm_hci.xml \
     manifest_audiocorehal_default.xml \
     manifest_btaudiocoreservices_qti.xml \
+    mapper.qti.xml \
     qasr_vintf.xml \
     snapdragon_services.xml \
     soundtrigger.qti.xml \
@@ -974,6 +1000,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth.btavsprovider.xml \
     vendor.qti.hardware.c2pa-aidl-service.xml \
     vendor.qti.hardware.capabilityconfigstore-service.xml \
+    vendor.qti.hardware.display.allocator-service.xml \
+    vendor.qti.hardware.display.composer-service3_v4.xml \
+    vendor.qti.hardware.display.demura-service.xml \
     vendor.qti.hardware.dsp-service.xml \
     vendor.qti.hardware.hexlp-service.xml \
     vendor.qti.hardware.minkipcbinder-service.xml \
@@ -1020,7 +1049,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.alarm-service \
     vendor.qti.hardware.c2pa-aidl-service-qti \
     vendor.qti.hardware.capabilityconfigstore-service \
+    vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.color-service \
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.demura-service \
     vendor.qti.hardware.minkipcbinder@1.0-service \
     vendor.qti.hardware.perf2-hal-service \
     vendor.qti.hardware.secureprocessor \
@@ -1044,6 +1076,7 @@ PRODUCT_PACKAGES += \
     init.kernel.post_boot \
     init.qcom.class_core \
     init.qcom.sensors \
+    init.qti.display_boot \
     init.qti.kernel.debug-alor \
     init.qti.kernel.debug-canoe \
     init.qti.kernel.debug-ftrace \
